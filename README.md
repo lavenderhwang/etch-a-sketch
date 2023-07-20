426496d34dbc979dc7d09106adbc95bd372634e7
Lavender <lhwang914@gmail.com>
2023-07-20 13:23:23 -0700

    Complete HTML + JS

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 7/17/23
 Soo....I kinda gave up on Rock Paper Scissors halfway through, so I'm determined to finish this one. I want a fully complete project I can be proud of!
+
+7/19/23
+well I def gave up on this one BUT i went back and finished RPS..so time to do this one too!
--- script.js
+++ script.js
@@ -1,23 +1,71 @@
 //SET IMPORTANT CONSTANTS + VARIABLES
-
-const grid = document.querySelector('.grid');
-
+let color = 'black';
+let click = true;
 //CREATE DEFAULT 16x16 GRID
 
-function defaultGrid() {
-	for (let i = 0; i < 16 * 16; i++) {
-		const gridCell = document.createElement('div');
-		gridCell.classList.add('grid-cell');
-		grid.appendChild(gridCell);
-	}
+function createGrid(size) {
+	let grid = document.querySelector('.grid');
+	let cells = grid.querySelectorAll('div');
+	cells.forEach((div) => div.remove());
+	grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+	grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-	gridCell.addEventListener('mouseover', (e) =>
-		e.target.classList.add('change-color')
-	);
+	for (let i = 0; i < size * size; i++) {
+		let cell = document.createElement('div');
+		cell.addEventListener('mouseover', colorSquare);
+		cell.classList.add('default-color');
+		grid.appendChild(cell);
+	}
 }
 
-defaultGrid();
+createGrid(16);
 
+/* CHANGE TO SLIDER FOR SIZE*/
+
+const sliderSize = document.querySelector('#rangeValue');
+function changeSize(input) {
+	createGrid(input);
+}
+
+/*function changeSize(input) {
+	if (input <= 2 || input <= 100) {
+		createGrid(input);
+	} else {
+		console.log('Please enter a number 2-100.');
+	}
+}*/
 //CHANGE COLOR ON HOVER//
 
-// LOAD THE PAGE //
+function colorSquare() {
+	if (click) {
+		if (color === 'rainbow') {
+			const rainbowColors = [
+				'red',
+				'orange',
+				'yellow',
+				'green',
+				'blue',
+				'purple',
+			];
+			this.style.backgroundColor =
+				rainbowColors[Math.floor(Math.random() * rainbowColors.length)];
+		} else {
+			this.style.backgroundColor = color;
+		}
+	}
+}
+
+function changeColor(choice) {
+	color = choice;
+}
+
+//RESET GRID//
+function resetGrid() {
+	let grid = document.querySelector('.grid');
+	let cells = grid.querySelectorAll('div');
+	cells.forEach((div) => (div.style.backgroundColor = 'white'));
+}
+
+document.querySelector('body').addEventListener('click', () => {
+	click = !click;
+});
